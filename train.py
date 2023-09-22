713f1570f5a14d0f97e3ad7ec4865cf00177e48a
michaelbabajide <babajidemichael67@gmail.com>
2023-09-22 10:44:54 +0100

    update default training epochs

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ def main():
     parser.add_argument('--architecture', type=str, default='vgg16', help='Model architectureitecture (e.g., vgg16)')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--hidden_units', type=int, default=512, help='Number of hidden units in the classifier')
-    parser.add_argument('--epochs', type=int, default=1, help='Number of training epochs')
+    parser.add_argument('--epochs', type=int, default=20, help='Number of training epochs')
     parser.add_argument('--gpu', action='store_true', help='Use GPU for training')
     args = parser.parse_args()
 
